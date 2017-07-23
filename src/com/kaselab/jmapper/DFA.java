5ed8077f2e0ff10b40a2b0d0360d97d9cfb77936
kaseDev <rick.kase@gmail.com>
2017-07-23 04:14:04 -0400

    Added functionlaity to read from saved DFA files and construct them in memory. Possible issue regarding special characters.

--- src/com/kaselab/jmapper/DFA.java
+++ src/com/kaselab/jmapper/DFA.java
@@ -3,6 +3,8 @@ package com.kaselab.jmapper;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -69,30 +71,138 @@ public class DFA {
 		return isSucceeded;
 	}
 
+
+	///// DFA parsing code /////
+
 	/**
 	 * fills the states array with a list of all the states
 	 * saved in the file passed as an argument.
 	 * @param file source of the DFA data
 	 */
 	private void readStates(File file) {
+		Iterator<String> i = IOHandler.readFileLineIterator(file);
+		String interpreterType = "";
+		String curString;
+		while (i.hasNext()) {
+			curString = i.next();
+			if (curString.equals("STATES") || curString.equals("TRANSITIONS")) {
+				interpreterType = curString;
+				continue;
+			}
+			if (interpreterType.equals("STATES"))
+				interpretAsState(curString);
+			else if (interpreterType.equals("TRANSITIONS"))
+				interpretAsTransition(curString);
+		}
+	}
 
+	/**
+	 * Interprets a string as a code that describes a State which
+	 * the DFA can be in adds that state to the list of possible
+	 * states.
+	 * @param code to be interpreted
+	 */
+	private void interpretAsState(String code) {
+		String[] values = code.split(",");
+		int index = Integer.valueOf(values[0].split(":")[1].trim());
+		String defaultState = values[1].split(":")[1].trim();
+		boolean isInitial = Boolean.valueOf(values[2].split(":")[1].trim());
+		boolean isFinal = Boolean.valueOf(values[3].split(":")[1].trim());
+		states.add(new State(index, isInitial, isFinal));
+		if (!defaultState.equals("null"))
+			states.get(index).setDefaultState(
+					states.get(Integer.valueOf(defaultState)));
+	}
+
+	/**
+	 * Interprets a string as a code that describes a transition between
+	 * two states that the DFA can be in adds the transition to the
+	 * appropriate state.
+	 *
+	 * ERROR may arise with the interpretation of special characters like
+	 * \n \t and so on.
+	 * @param code to be interpreted
+	 */
+	private void interpretAsTransition(String code) {
+		String[] values = code.split(",");
+		// prepare indices
+		int firstStateIndex = Integer.valueOf(values[0].split(":")[1].trim());
+		int secondStateIndex = Integer.valueOf(values[1].split(":")[1].trim());
+		// prepare triggers
+		String triggerString = values[2].split(":")[1].trim();
+		triggerString = triggerString.substring(1, triggerString.length() - 1);
+		String[] triggers = triggerString.split(" ");
+		// enter state transitions
+		for (String trigger : triggers)
+			states.get(firstStateIndex).addStateTransition(toChar(trigger),
+					states.get(secondStateIndex));
+	}
+
+	/**
+	 * simple parser for the characters that will be encountered in the
+	 * DFA files to the characters that they actually represents.
+	 * @param str representing a single character
+	 * @return the character the string is meant to represent
+	 */
+	private char toChar(String str) {
+		switch (str) {
+			case "\\n":
+				return System.getProperty("line.separator").charAt(0);
+			default:
+				return str.charAt(0);
+		}
+	}
+
+	///// Debugging and clarity Code /////
+
+	@Override
+	public String toString() {
+//		if (states.size() == 0)
+//			return "empty DFA";
+		StringBuilder build = new StringBuilder();
+		List<Character> triggers;
+		for (State state : states) {
+			build.append(state);
+			build.append("\n");
+			for (Map.Entry<State, List<Character>> connection
+					: state.getStateMapping().entrySet()) {
+				triggers = connection.getValue();
+				build.append("\tConnected to: ");
+				build.append(connection.getKey().getId());
+				build.append(", Triggered by: ");
+				for (int i = 0; i < triggers.size(); i++) {
+					build.append(triggers.get(i));
+					if (i < triggers.size() - 1)
+						build.append(", ");
+				}
+			}
+			build.append("\n");
+		}
+		return build.toString();
 	}
 
 
-
+	/**
+	 * Represents an individual state that the DFA can be in as
+	 * well as all the transitions possible within the state.
+	 */
 	private class State {
+		private int id;
 		private boolean isFinal;
 		private boolean isInitial;
 		private Map<Character, State> stateMap = new HashMap<>();
+		private State defaultState = null;
 
 		/**
 		 * Creates a state for to be used in a DFA. whether or not
 		 * this is an initial state or final state is determined at
 		 * creation and cannot be changed.
+		 * @param id identifier for human debugging and understanding
 		 * @param isInitial whether or not this is the initial state
 		 * @param isFinal whether or not this is a final state
 		 */
-		public State(boolean isInitial, boolean isFinal) {
+		private State(int id, boolean isInitial, boolean isFinal) {
+			this.id = id;
 			this.isInitial = isInitial;
 			this.isFinal = isFinal;
 		}
@@ -103,7 +213,7 @@ public class DFA {
 		 * @param chr to direct to the given state
 		 * @param state to be directed to by the given char
 		 */
-		public void addStateTransition(char chr, State state) {
+		private void addStateTransition(char chr, State state) {
 			stateMap.put(chr, state);
 		}
 
@@ -113,9 +223,20 @@ public class DFA {
 		 * @param chrs to direct to the given state
 		 * @param state to be directed to by the given chars
 		 */
-		public void addStateTransition(char[] chrs, State state) {
+		private void addStateTransition(char[] chrs, State state) {
 			for (char chr : chrs)
 				addStateTransition(chr, state);
+		}
+
+		/**
+		 * sets the state that should be pointed too if the an
+		 * unmapped char is passed. this value is null unless
+		 * otherwise set.
+		 * @param defaultState to be pointed to when unmapped keys are
+		 *                     processed.
+		 */
+		private void setDefaultState(State defaultState) {
+			this.defaultState = defaultState;
 		}
 
 		/**
@@ -124,7 +245,7 @@ public class DFA {
 		 * @return boolean valued indicating that this state is or
 		 * is not a final state.
 		 */
-		public boolean isFinal() {
+		private boolean isFinal() {
 			return isFinal;
 		}
 
@@ -133,19 +254,66 @@ public class DFA {
 		 * @return boolean valued indicating that this state is or
 		 * is not the initial state.
 		 */
-		public boolean isInitial() {
+		private boolean isInitial() {
 			return isInitial;
 		}
 
 		/**
-		 * Returns the next state given a certain char as
-		 * input.
+		 * returns the ID of this state for human inspection.
+		 * @return the id of this state
+		 */
+		private int getId() {
+			return id;
+		}
+
+		/**
+		 * Returns the next state given a certain char as input. If
+		 * the specified char is not valid then returns the state
+		 * specified as default. In no state was specified as default
+		 * then returns null.
 		 * @param chr character to determine the next state
 		 *            of the DFA.
 		 * @return the next state specified.
 		 */
-		public State findNextState(char chr) {
-			return stateMap.get(chr);
+		private State findNextState(char chr) {
+			State state = stateMap.get(chr);
+			return (state != null) ? state : defaultState;
+		}
+
+		/**
+		 * Takes the one to one mapping of characters to States and
+		 * reverses it to become a one to many mapping of States to
+		 * characters.
+		 * @return Map that maps all the States this state can
+		 * transition to with a list of all the characters that will
+		 * cause that transition
+		 */
+		private Map<State, List<Character>> getStateMapping() {
+			Map<State, List<Character>> reverseMap = new HashMap<>();
+			State curState;
+			for (Map.Entry<Character, State> entry : stateMap.entrySet()) {
+				curState = entry.getValue();
+				if (!reverseMap.containsKey(curState))
+					reverseMap.put(curState, new LinkedList<>());
+				reverseMap.get(curState).add(entry.getKey());
+			}
+			return reverseMap;
+		}
+
+		/**
+		 * returns a String representation of the State.
+		 * @return String representation of the State
+		 */
+		@Override
+		public String toString() {
+			StringBuilder build = new StringBuilder("State[");
+			build.append("ID: " + id);
+			build.append(", Initial: " + isInitial);
+			build.append(", Final: " + isFinal);
+			build.append(", Default transition: ");
+			build.append((defaultState != null) ? defaultState.id : null);
+			build.append("]");
+			return build.toString();
 		}
 	}
 }
