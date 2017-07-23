4db6a216826150bdd35f942f102e290641bcd6b5
kaseDev <rick.kase@gmail.com>
2017-07-23 00:55:14 -0400

    Updated IOHandler to handle a wider range of file reading options.

--- src/com/kaselab/jmapper/IOHandler.java
+++ src/com/kaselab/jmapper/IOHandler.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public class IOHandler {
 
-	public static Iterator<Character> readFile(File file) {
+	public static List<Character> readFileCharList(File file) {
 		List<Character> charList = new ArrayList<>();
 		BufferedReader reader;
 		int curChar;
@@ -21,6 +21,28 @@ public class IOHandler {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return charList.iterator();
+		return charList;
+	}
+
+	public static Iterator<Character> readFileCharIterator(File file) {
+		return readFileCharList(file).iterator();
+	}
+
+	public static List<String> readFileLineList(File file) {
+		BufferedReader reader;
+		List<String> lineList = new ArrayList<>();
+		String curLine;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			while ((curLine = reader.readLine()) != null)
+				lineList.add(curLine);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lineList;
+	}
+
+	public static Iterator<String> readFileLineIterator(File file) {
+		return readFileLineList(file).iterator();
 	}
 }
