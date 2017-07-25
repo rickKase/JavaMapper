899a17559f5fd1954d9928fbc4bc46b47b40678a
kaseDev <rick.kase@gmail.com>
2017-07-25 16:06:55 -0400

    Moved the State class into it's own file.

--- src/com/kaselab/jmapper/DFA.java
+++ src/com/kaselab/jmapper/DFA.java
@@ -2,9 +2,7 @@ package com.kaselab.jmapper;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +11,8 @@ import java.util.Map;
  */
 public class DFA {
 
-	private List<State> states = new ArrayList<State>();
+	private List<State> states = new ArrayList<>();
+	private List<State> finalStates = new ArrayList<>();
 	private State initialState;
 	private State currentState;
 	private boolean isChecking;
@@ -143,6 +142,8 @@ public class DFA {
 		boolean isInitial = Boolean.valueOf(values[2].split(":")[1].trim());
 		boolean isFinal = Boolean.valueOf(values[3].split(":")[1].trim());
 		states.add(new State(index, isInitial, isFinal));
+		if (isFinal)
+			finalStates.add(states.get(index));
 		if (!defaultState.equals("null"))
 			states.get(index).setDefaultState(
 					states.get(Integer.valueOf(defaultState)));
@@ -233,144 +234,5 @@ public class DFA {
 		}
 		build.append("\n");
 		return build.toString();
-	}
-
-	////////////////////////////////
-	///// Internal State class /////
-	////////////////////////////////
-
-	/**
-	 * Represents an individual state that the DFA can be in as
-	 * well as all the transitions possible within the state.
-	 */
-	private class State {
-		private int id;
-		private boolean isFinal;
-		private boolean isInitial;
-		private Map<Character, State> stateMap = new HashMap<>();
-		private State defaultState = null;
-
-		/**
-		 * Creates a state for to be used in a DFA. whether or not
-		 * this is an initial state or final state is determined at
-		 * creation and cannot be changed.
-		 * @param id identifier for human debugging and understanding
-		 * @param isInitial whether or not this is the initial state
-		 * @param isFinal whether or not this is a final state
-		 */
-		private State(int id, boolean isInitial, boolean isFinal) {
-			this.id = id;
-			this.isInitial = isInitial;
-			this.isFinal = isFinal;
-		}
-
-		/**
-		 * Adds the character as valid transitions to the indicated
-		 * state.
-		 * @param chr to direct to the given state
-		 * @param state to be directed to by the given char
-		 */
-		private void addStateTransition(char chr, State state) {
-			stateMap.put(chr, state);
-		}
-
-		/**
-		 * Adds all the characters in the char[] as valid transitions
-		 * to the identified state.
-		 * @param chrs to direct to the given state
-		 * @param state to be directed to by the given chars
-		 */
-		private void addStateTransition(char[] chrs, State state) {
-			for (char chr : chrs)
-				addStateTransition(chr, state);
-		}
-
-		/**
-		 * Sets the state that should be pointed too if the an
-		 * unmapped char is passed. this value is null unless
-		 * otherwise set.
-		 * @param defaultState to be pointed to when unmapped keys are
-		 *                     processed.
-		 */
-		private void setDefaultState(State defaultState) {
-			this.defaultState = defaultState;
-		}
-
-		/**
-		 * Returns whether or not this state is set to be a valid
-		 * final state.
-		 * @return boolean valued indicating that this state is or
-		 * is not a final state.
-		 */
-		private boolean isFinal() {
-			return isFinal;
-		}
-
-		/**
-		 * Returns whether or not this state is set to be initial.
-		 * @return boolean valued indicating that this state is or
-		 * is not the initial state.
-		 */
-		private boolean isInitial() {
-			return isInitial;
-		}
-
-		/**
-		 * Returns the ID of this state for human inspection.
-		 * @return the id of this state
-		 */
-		private int getId() {
-			return id;
-		}
-
-		/**
-		 * Returns the next state given a certain char as input. If
-		 * the specified char is not valid then returns the state
-		 * specified as default. In no state was specified as default
-		 * then returns null.
-		 * @param chr character to determine the next state
-		 *            of the DFA.
-		 * @return the next state specified.
-		 */
-		private State findNextState(char chr) {
-			State state = stateMap.get(chr);
-			return (state != null) ? state : defaultState;
-		}
-
-		/**
-		 * Takes the one to one mapping of characters to States and
-		 * reverses it to become a one to many mapping of States to
-		 * characters.
-		 * @return Map that maps all the States this state can
-		 * transition to with a list of all the characters that will
-		 * cause that transition
-		 */
-		private Map<State, List<Character>> getStateMapping() {
-			Map<State, List<Character>> reverseMap = new HashMap<>();
-			State curState;
-			for (Map.Entry<Character, State> entry : stateMap.entrySet()) {
-				curState = entry.getValue();
-				if (!reverseMap.containsKey(curState))
-					reverseMap.put(curState, new LinkedList<>());
-				reverseMap.get(curState).add(entry.getKey());
-			}
-			return reverseMap;
-		}
-
-		/**
-		 * Returns a String representation of the State.
-		 * @return String representation of the State
-		 */
-		@Override
-		public String toString() {
-			StringBuilder build = new StringBuilder("State[");
-			build.append("ID: " + id);
-			build.append(", Initial: " + isInitial);
-			build.append(", Final: " + isFinal);
-			build.append(", Default transition: ");
-			build.append((defaultState != null) ? defaultState.id : null);
-			build.append("]");
-			return build.toString();
-		}
 	}
 }
